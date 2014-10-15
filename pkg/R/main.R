ad6ce1e8dbdfc883ed21e541469fae1004b429bd
Thibaut Jombart <thibautjombart@gmail.com>
2014-10-15 15:45:44 +0100

    code for infectiousness in all patches OK

--- pkg/R/main.R
+++ pkg/R/main.R
@@ -6,19 +6,43 @@ x <- data.frame(onset=sample(as.Date("2014-01-01")+0:10, 30, replace=TRUE), patc
 
 
 
-model <- function(x){
-    ## CHECKS ##
-    ## if(!require(OutbreakTools)) stop("OutbreakTools is needed")
+model <- function(x, D.patches=NULL, w){
+    ## CHECKS / HANDLE ARGUMENTS ##
     x <- na.omit(x)
     x$patch <- factor(x$patch)
     patches <- levels(x$patch)
     n.patches <- length(patches)
 
-    ## COMPUTE INCIDENCE ##
+    ## handle empty distance matrix between patches
+    if(is.null(D.patches)){
+        D.patches <- matrix(0, ncol=n.patches,nrow=n.patches)
+        colnames(D.patches) <- rownames(D.patches) <- patches
+    }
+    ## force diagonal to zero
+    diag(D.patches) <- 0
+
+    ## useful warnings/errors
+    if(any(D.patches)<0) warning("D.patches has negative entries")
+    if(ncol(D.patches)!= nrow(D.patches)) stop(paste("D.patches is not square",
+           nrow(D.patches),"rows,",
+           ncol(D.patches), "columns."))
+    if(ncol(D.patches)!= n.patches) stop(paste("D.patches has wrong dimensions: expected",
+           n.patches, "but got",
+           ncol(D.patches)))
+
+
+    ## HANDLE DATES
     date.range <- range(x$onset)
     all.dates <- seq(date.range[1],date.range[2],by=1)
     n.dates <- length(all.dates)
 
+    ## HANDLE SERIAL INTERVAL DISTRIBUTION
+    ## note: w[i] is the proba of infection after i days
+    w <- w/sum(w) # standardize distri to 1
+    w <- c(w, rep(0, n.dates))
+
+
+    ## COMPUTE INCIDENCE ##
     ## function to get daily incidence
     get.daily.incid <- function(dates, from, to){
         out <- table(factor(as.character(dates), levels=as.character(seq(from, to, by=1))))
@@ -36,11 +60,26 @@ model <- function(x){
                         incidence=unlist(temp))
 
 
-    ## FUNCTION TO GET THE LOG-LIKELIHOOD ##
-    ## Poisson likelihood
+    ## FUNCTIONS TO GET RATES AND LOG-LIKELIHOOD ##
+    ## POISSON LIKELIHOOD
     ## p(y|rates)
     get.LL <- function(y, rates){
         return(sum(dpois(y, rates, log=TRUE)))
     }
+
+    ## GET BETA FOR A PATCH ##
+    ## get infectiousness for day 'date' and
+    ## a vector of onset dates 'onsets'
+    get.infec <- function(date, onsets){
+        temp <- as.integer(date-onsets)
+        out <- sum(w[temp[temp>0]])
+        return(out)
+    }
+
+    ## 'infec.mat' contains the sum of infectiousness for each day (row) and patch (column)
+    infec.mat <- t(sapply(all.dates, function(t) tapply(x$onset, x$patch, function(e) get.infec(t, e))))
+    
+
+
 
 }
