7625d1c9558d3d7c086846efe21841827a4ae59c
Thibaut Jombart <thibautjombart@gmail.com>
2014-10-21 08:57:04 +0100

    forgot to commit that one

--- pkg/R/testing.R
+++ pkg/R/testing.R
@@ -1,7 +1,7 @@
 
 
 ## simulate toy data
-x <- data.frame(onset=sample(as.Date("2014-01-01")+0:100, 2000, replace=TRUE), patch=sample(c('a','b','c','d','e'), replace=TRUE, 2000))
+x <- data.frame(onset=sample(as.Date("2014-01-01")+0:30, 2000, replace=TRUE), patch=sample(c('a','b','c','d','e'), replace=TRUE, 2000))
 
 
 ## run MCMC
@@ -9,6 +9,7 @@ system.time(res <- epidemicMCMC(x,w=c(1,2,1),n.iter=1e5,sample.every=500))
 
 ## make some graphs
 pdf("alltraces.pdf")
+par(mfrow=c(3,2))
 for(i in 2:7) plot(res$chains[-1,1], res$chains[-1,i], main=names(res$chains)[i],type="l",xlab="",ylab="")
 dev.off()
 
